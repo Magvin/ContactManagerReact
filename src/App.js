74fc3111f625e73b923ae4495016c91539fde166
magvin <zans.litinskis@gmail.com>
2018-08-24 21:23:35 +0100

    two new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="Jdoe@gmail.com" phone="+3712225458" />
       </div>
     );
   }
